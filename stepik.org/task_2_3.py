037a76f2f5ef5cd308822d477efef0ccbf20e69b
Anton Baranouski <Baranouski_AG@local.st.by>
2017-01-05 13:59:21 +0300

    task correct work. done

--- stepik.org/task_2_3.py
+++ stepik.org/task_2_3.py
@@ -1,20 +1,32 @@
 class multifilter:
-    def __iter__(self): 
-        for ff in self.funcs:
-            for i in self.iterable:
+    def __iter__(self):         
+        for i in self.iterable:
+            self.pos = 0
+            self.neg = 0
+            for ff in self.funcs:    
                 if ff(i):
-                    yield i
+                    self.pos += 1
+                else:
+                    self.neg += 1
         
+            if self.judge(self.pos, self.neg):        
+                yield i
         
-    def __init__(self, iterable, *funcs):
-        self.iterable = iterable        
-        self.counter = -1
-        self.funcs = funcs
-            
     def judge_any(pos, neg):
-        pass
-        
-    #def __next__(self):
+        return pos >= 1
+
+    def judge_half(pos, neg):
+        return pos >= neg    
+
+    def judge_all(pos, neg):
+        return neg == 0
+    
+    def __init__(self, iterable, *funcs, judge=judge_any):
+        self.iterable = iterable                
+        self.funcs = funcs
+        self.judge = judge
+        self.pos = 0
+        self.neg = 0    
         
             
 def mul2(x):    
@@ -22,7 +34,10 @@ def mul2(x):
 
 def mul3(x):    
     return x % 3 == 0
-    
-a = [i for i in range(1, 10)]
 
-print(list(multifilter(a, mul2, mul3)))
+def mul5(x):
+    return x % 5 == 0
+    
+a = [i for i in range(31)] # [0, 1, 2, ... , 30]
+
+print(list(multifilter(a, mul2, mul3, mul5, judge=multifilter.judge_half)))
