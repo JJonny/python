507ed9561872aad8805045fe350b73dd0c6ef688
Anton Baranouski <Baranouski_AG@local.st.by>
2017-01-09 17:41:59 +0300

    task complite

--- stepik.org/3_4_6.py
+++ stepik.org/3_4_6.py
@@ -1,4 +1,4 @@
-import requests
+import requests as rq
 import sys
 import re
 
@@ -6,32 +6,21 @@ sys.stdin = open('input_3_4_6.txt')
 
 start = input()
 end = input()
+ 
+pattern = "href *= *[\'\"](.*?)[\'\"]"
+ 
+try:
+  r = rq.get(start)
+  for ref in re.findall(pattern, r.text):
+    try:
+      r = rq.get(ref)
+      if end in re.findall(pattern, r.text):
+        print("Yes")
+        break
+    except:
+      pass
+  else:        
+    print("No")
+except:
+  print("No")
 
-
-def find_href(request_res):
-	o = re.compile(r'href="(.*?)"')	
-	found = o.findall(request_res.text)		
-	return found
-
-def moveToLink2(link):
-	res = requests.get(link)	
-	if res.status_code == 200:	
-		out = find_href(res)
-		if end in out:
-			return 'Yes'
-		else:
-			return 'No'
-
-
-res = requests.get(start)	
-if res.status_code == 200:
-	hrefs = find_href(res)		
-	for i in hrefs:		
-		r = moveToLink2(i)
-		print(r)
-		
-
-
-
-	
-	
