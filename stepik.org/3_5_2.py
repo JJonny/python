696349c4ed55a6138900fe08f0fb4d5d128e0173
Anton Baranouski <Baranouski_AG@local.st.by>
2017-01-09 19:36:22 +0300

    task complite

---
+++ stepik.org/3_5_2.py
@@ -0,0 +1,26 @@
+import csv
+
+d = dict()
+with open('Crimes.csv') as f:
+	reader = csv.DictReader(f)
+
+	
+	for row in reader:
+		
+		date = row['Date'][6:10]
+		t = row['Primary Type']
+		
+		val = 1
+		try:
+			if '2015' in date:
+				if t in d.keys():
+					d[t] += 1
+				else:
+					d[t] = val
+		except:
+			print('EEEEERR')
+			print(d)
+			break
+print(d)
+	# for k,v in d.items():
+	# 	print(k,v)
